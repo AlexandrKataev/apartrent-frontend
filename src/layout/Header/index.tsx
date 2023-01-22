e28f64cf484708a42450f8adf26cbb3c77314a66
AlexandrKataev <dkeyman@mail.ru>
2023-01-22 18:44:17 +0300

    fix popup closing

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -5,26 +5,10 @@ import { ReactComponent as UserIcon } from 'shared/icons/user.svg';
 import { ReactComponent as DownIcon } from 'shared/icons/down.svg';
 import { Popup } from 'shared/ui/Popup';
 
-type PopupClick = MouseEvent & {
-  path: Node[];
-};
-
 export const Header: React.FC = () => {
   const navigate = useNavigate();
-  const popupRef = React.useRef<HTMLDivElement>(null);
+
   const [showPopup, setShowPopup] = useState(false);
-
-  React.useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const _event = event as PopupClick;
-      if (popupRef.current && !_event.path.includes(popupRef.current)) {
-        setShowPopup(false);
-      }
-    };
-
-    document.body.addEventListener('click', handleClickOutside);
-    return () => document.body.removeEventListener('click', handleClickOutside);
-  }, []);
 
   const onClickHome = () => {
     navigate(`/`);
@@ -38,11 +22,11 @@ export const Header: React.FC = () => {
       <div className={s.home} onClick={onClickHome}>
         apartRent
       </div>
-      <div ref={popupRef} className={s.user} onClick={onClickUser}>
+      <div className={s.user} onClick={onClickUser}>
         User Name
         <UserIcon className={s.user_icon} />
         <DownIcon className={s.down_icon} />
-        {showPopup && <Popup />}
+        {showPopup && <Popup setShowPopup={setShowPopup} showPopup={showPopup} />}
       </div>
     </div>
   );
--- src/shared/ui/Popup/index.tsx
+++ src/shared/ui/Popup/index.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import s from './Popup.module.scss';
 
-export const Popup = () => {
+interface PopupProps {
+  showPopup: boolean;
+  setShowPopup: (showPopup: boolean) => void;
+}
+
+export const Popup: FC<PopupProps> = ({ showPopup, setShowPopup }) => {
   const navigate = useNavigate();
+  const popupRef = useRef<HTMLDivElement>(null);
 
   const onClickProfile = () => {
     navigate('/profile');
@@ -13,8 +19,26 @@ export const Popup = () => {
     navigate('/admin');
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        showPopup &&
+        popupRef.current &&
+        !popupRef.current.contains(event.target as Node | null)
+      ) {
+        setShowPopup(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showPopup]);
+
   return (
-    <div className={s.container}>
+    <div className={s.container} ref={popupRef}>
       <div className={s.item} onClick={onClickProfile}>
         Профиль
       </div>
